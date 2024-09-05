0fd577d38fbd95a0ef45b2841012c3ebd006734d
odiljon1334 <odil1334@gmail.com>
2024-09-05 18:40:45 +0500

    feat: H-TASK is DONE! @MITASK

--- src/train.ts
+++ src/train.ts
@@ -1,22 +1,32 @@
 console.log("TRAIN AREA!");
+
+// H-TASK
+//define
+function getPositive(arrInt: number[]) {
+    const arr = arrInt.filter(num => num > 0).join('');
+    return arr;
+}
+const result = getPositive([1, -4, 2]);
+console.log('H-TASK:', result);
+
  // G-TASK
  // Define
-function getHighestIndex(arr: number []) {
-    let index = 0;
+// function getHighestIndex(arr: number []) {
+//     let index = 0;
 
-    for(let i = 0; i < arr.length; i++){
-        // agar arraymizni  [i]nchi indexi katta bo'lsa arrayimizni indexdan
-        // u katta emas for loop ishga tushadi.
-        // endi arrayimizni [i]nchi indexi 1ga teng u katta emas.
-        // for loop ishga tushmedi.  
-        if(arr[i] > arr[index]) {
-            // yuqorida xosil qilib olgan o'zgaruvchimizni qiymati 0 ga tenglab olgandik
-            // endi [i]ning qiymati 1 ga teng indeximizni qiymatini i ga tenglab olib uni return qilib olamiz.
-            index = i;
-        }
-    }
-    return index;
-}
- // Call
-const result = getHighestIndex([5, 21, 12, 21, 8]);
-console.log('G-TASK:', result);
+//     for(let i = 0; i < arr.length; i++){
+//         // agar arraymizni  [i]nchi indexi katta bo'lsa arrayimizni indexdan
+//         // u katta emas for loop ishga tushadi.
+//         // endi arrayimizni [i]nchi indexi 1ga teng u katta emas.
+//         // for loop ishga tushmedi.  
+//         if(arr[i] > arr[index]) {
+//             // yuqorida xosil qilib olgan o'zgaruvchimizni qiymati 0 ga tenglab olgandik
+//             // endi [i]ning qiymati 1 ga teng indeximizni qiymatini i ga tenglab olib uni return qilib olamiz.
+//             index = i;
+//         }
+//     }
+//     return index;
+// }
+//  // Call
+// const result = getHighestIndex([5, 21, 12, 21, 8]);
+// console.log('G-TASK:', result);
