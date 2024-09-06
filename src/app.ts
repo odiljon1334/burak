2431718306c144b3495aeef22a2f27234d23f5aa
odiljon1334 <odil1334@gmail.com>
2024-09-07 01:15:20 +0500

    feat: create routing and member controller

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,6 @@
 import express from "express";
 import path from "path";
+import router from "./router";
 
 /** 1-ENTRANCE **/
 const app = express();
@@ -14,6 +15,7 @@ app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
 /** 4-ROUTERS **/
+app.use('/', router); // MiddleWare Design Pattern
 
 
 export default app;
--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,9 @@
-import mongoose from "mongoose";
 import dotenv from "dotenv";
 dotenv.config();
+
+import mongoose from "mongoose";
 import app from "./app";
+
 
 mongoose.connect(process.env.MONGO_URL as string, {})
 .then((data) => {
