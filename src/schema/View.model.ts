8a2c3457c181b4fe51e2795e3bfdf2dd0ead0f62
odiljon1334 <odil1334@gmail.com>
2024-10-23 01:29:37 +0500

    feat: create view schema and service model

---
+++ src/schema/View.model.ts
@@ -0,0 +1,23 @@
+import mongoose, { Schema } from "mongoose";
+import { ViewGroup } from "../libs/enums/view.enum";
+
+const viewSchema = new Schema({
+    viewGroup: {
+        type: String,
+        enum: ViewGroup,
+        required: true,
+    },
+
+    memberId: {
+        type: Schema.Types.ObjectId,
+        required: true,
+        ref: "Member",
+    },
+
+    viewRefId: {
+        type: Schema.Types.ObjectId,
+        required: true,
+    }
+}, { timestamps: true });
+
+export default mongoose.model('View', viewSchema);
