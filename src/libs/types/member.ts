1658bf92565da2e4ca60defe80fcc4a294af0895
odiljon1334 <odil1334@gmail.com>
2024-11-02 17:22:48 +0500

    feat: MITASK is Done

--- src/libs/types/member.ts
+++ src/libs/types/member.ts
@@ -1,5 +1,5 @@
 import { ObjectId } from "mongoose"
-import { MemberStatus, MemberType } from "../enums/member.enum";
+import { MemberStatus, MemberType } from "../enums/member.enum"
 import { Request } from "express";
 import { Session } from "express-session";
 
--- src/train.ts
+++ src/train.ts
@@ -42,12 +42,25 @@ console.log("TRAIN AREA!");
 // Shunday function yozing, u array ichidagi objectlarni “age” qiymati boyicha sortlab bersin.
 // MASALAN: sortByAge([{age:23}, {age:21}, {age:13}]) return [{age:13}, {age:21}, {age:23}]
 
-function sortByAge(arr: { age: number }[]) {
-    return arr.slice().sort((a, b) => a.age - b.age);
-}
+// function sortByAge(arr: { age: number }[]) {
+//     return arr.slice().sort((a, b) => a.age - b.age);
+// }
+//
+// const result = sortByAge([{age:23}, {age:21}, {age:13}]);
+// console.log('result: =>', result);
 
-const result = sortByAge([{age:23}, {age:21}, {age:13}]);
+// ZB-TASK:
+
+// Shunday function yozing, uni 2 ta number parametri bolsin va berilgan sonlar orasidan random raqam return qilsin
+// MASALAN: randomBetween(30, 50) return 45
+function randomBetween(min: number, max: number): number {
+    return (Math.random() * (max - min + 1) + min) | 0;
+}
+// Call
+const result = randomBetween(30, 50);
 console.log('result: =>', result);
+  
+
 
 
 // Z-TASK:
