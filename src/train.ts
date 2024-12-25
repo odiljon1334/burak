c5a27fda6737703e65d2ce40eb7d4c88a0f8bca8
odiljon1334 <odiljon9507@gmail.com>
2024-12-25 13:47:54 +0500

    feat: develop MITASK

--- src/train.ts
+++ src/train.ts
@@ -38,20 +38,55 @@ console.log("TRAIN AREA!");
 // VALIDATION: FRONTEND vs PIPE vs BACKEND vs DB
 // VALIDATION: CLIENT vs DTO vs SERVER vs SCHEMA
 
+// ZU-TASK:
+
+// Shunday function yozing, uni 2 ta parametri bo'lsin, biri array va biri string. Function arrayda berilgan malumotlarni 2-parametrdagi string qiymati asosida guruhlab qaytarsin.
+// MASALAN:
+// const data = [
+//   { name: 'Alice', age: 25, city: 'London' },
+//   { name: 'Bob',   age: 30, city: 'New York' },
+//   { name: 'Charlie', age: 25, city: 'London' },
+// ];
+// console.log(groupBy(data, 'city')); // { 'London': [ { name: 'Alice', age: 25, city: 'London' }, { name: 'Charlie', age: 25, city: 'London' } ], 'New York': [ { name: 'Bob', age: 30, city: 'New York' } ] }
+
+interface T  {
+    [key: string]: any;
+}
+
+function groupBy(array: T[], key: keyof T) {
+    const result: T = {};
+    for (const item of array) {
+      const groupKey = String(item[key]);
+      if (!result[groupKey]) {
+        result[groupKey] = [];
+      }
+      result[groupKey].push(item);
+    }
+    return result;
+  }
+
+  const data = [
+    { name: 'Alice', age: 25, city: 'London' },
+    { name: 'Bob', age: 30, city: 'New York' },
+    { name: 'Charlie', age: 25, city: 'London' },
+  ];
+  console.log(groupBy(data, 'city'));
+  
+  
+
+
 // ZT-TASK:
-
-
 // Shunday function yozing, u parametridagi array ichidagi barcha nollarni array oxiriga qoyib qolgan raqamlar ketma-ketligini saqlasin.
 // MASALAN: moveZeroes([0, 1, 0, 3, 12]) return [1, 3, 12, 0, 0]
 
-function moveZeroes(nums: number[]): number[] {
-    const nonZeroes = nums.filter(num => num !== 0);
-    const zeroes = nums.filter(num => num === 0);
-    return [...nonZeroes, ...zeroes];
-}
+// function moveZeroes(nums: number[]): number[] {
+//     const nonZeroes = nums.filter(num => num !== 0);
+//     const zeroes = nums.filter(num => num === 0);
+//     return [...nonZeroes, ...zeroes];
+// }
 
-const result = moveZeroes([0, 1, 0, 3, 12]);
-console.log('result: => ', result);
+// const result = moveZeroes([0, 1, 0, 3, 12]);
+// console.log('result: => ', result);
 
 
 // function firstUniqueCharIndex(str: string): number {
