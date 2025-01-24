b5c060e8ed48e74820c58f475ba7234a18f42cd3
odiljon1334 <odiljon9507@gmail.com>
2025-01-25 04:59:27 +0500

    fix: modify productCollection add new type

--- src/libs/enums/product.enum.ts
+++ src/libs/enums/product.enum.ts
@@ -1,28 +1,29 @@
 export enum ProductSize {
-    SMALL = 'SMALL',
-    NORMAL = 'NORMAL',
-    LARGE = 'LARGE',
-    SET = 'SET'
+  SMALL = "SMALL",
+  NORMAL = "NORMAL",
+  LARGE = "LARGE",
+  SET = "SET",
 }
 
 export enum ProductVolume {
-    HALF = 0.5,
-    ONE = 1,
-    ONE_POINT_TWO = 1.2,
-    ONE_POINT_FIVE = 1.5,
-    TWO = 2,
+  HALF = 0.5,
+  ONE = 1,
+  ONE_POINT_TWO = 1.2,
+  ONE_POINT_FIVE = 1.5,
+  TWO = 2,
 }
 
 export enum ProductStatus {
-    PAUSE = 'PAUSE',
-    PROCESS = 'PROCESS',
-    DELETE = 'DELETE',
+  PAUSE = "PAUSE",
+  PROCESS = "PROCESS",
+  DELETE = "DELETE",
 }
 
 export enum ProductCollection {
-    DISH = 'DISH',
-    SALAD = 'SALAD',
-    DESSERT = 'DESSERT',
-    DRINK = 'DRINK',
-    OTHER = 'OTHER',
+  DISH = "DISH",
+  PIZZA = "PIZZA",
+  SALAD = "SALAD",
+  DESSERT = "DESSERT",
+  DRINK = "DRINK",
+  OTHER = "OTHER",
 }
