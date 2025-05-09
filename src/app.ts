122d6187b622d510528290dfe3a3ba5c8cdd2281
odiljon1334 <odiljon9507@gmail.com>
2025-05-09 05:47:43 +0500

    feat: develop socketIO server

--- src/app.ts
+++ src/app.ts
@@ -6,6 +6,8 @@ import routerAdmin from "./router-admin";
 import morgan from "morgan";
 import cookieParser from "cookie-parser";
 import { MORGAN_FORMAT } from "./libs/config";
+import { Server as ServerIOServer } from "socket.io";
+import http from "http";
 
 import session from "express-session";
 import ConnectMongoDB from "connect-mongodb-session";
@@ -14,50 +16,70 @@ import { T } from "./libs/types/common";
 const MongoDBStore = ConnectMongoDB(session);
 // Bu yerda MongoDBStore class'sidan  (namuna)  instance olyapmiz
 const store = new MongoDBStore({
-    uri: String(process.env.MONGO_URL),// mongoDbga ulanish
-    collection: 'sessions', // MongoDB Databasesimizda session xosil qilib beradi.
+  uri: String(process.env.MONGO_URL), // mongoDbga ulanish
+  collection: "sessions", // MongoDB Databasesimizda session xosil qilib beradi.
 });
 
 /** 1-ENTRANCE **/
 // MiddleWare Design Patterns
 const app = express();
-app.use(express.static(path.join(__dirname, 'public')));
-app.use("/uploads", express.static('./uploads')); // uploads folderini ham middlware orqalik clieantga ochib berdik.
+app.use(express.static(path.join(__dirname, "public")));
+app.use("/uploads", express.static("./uploads")); // uploads folderini ham middlware orqalik clieantga ochib berdik.
 app.use(express.urlencoded({ extended: true })); // Traditional API
 app.use(express.json()); // REST API
-app.use(cors({
+app.use(
+  cors({
     credentials: true,
-     origin: true
-    })
+    origin: true,
+  })
 );
 app.use(cookieParser());
 app.use(morgan(MORGAN_FORMAT));
 
 /** 2-SESSIONS **/
 app.use(
-    session({
-        secret: String(process.env.SESSION_SECRET),
-        cookie: {
-            maxAge: 1000 * 3600 * 3, // 3h
-        },
-        store: store,
-        resave: true, // Agar false bo'lsa  10:30 da uath => 13:30 auth tugaydi. 12:00 da saytga qayta kirsak auth yangilanmaydi.
-        saveUninitialized: true,
-    })
+  session({
+    secret: String(process.env.SESSION_SECRET),
+    cookie: {
+      maxAge: 1000 * 3600 * 3, // 3h
+    },
+    store: store,
+    resave: true, // Agar false bo'lsa  10:30 da uath => 13:30 auth tugaydi. 12:00 da saytga qayta kirsak auth yangilanmaydi.
+    saveUninitialized: true,
+  })
 );
 
-app.use(function(req, res, next) {
-    const sessionInstanc = req.session as T;
-    res.locals.member = sessionInstanc.member;
-    next();
-})
+app.use(function (req, res, next) {
+  const sessionInstanc = req.session as T;
+  res.locals.member = sessionInstanc.member;
+  next();
+});
 
 /** 3-VIEWS **/
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'ejs');
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "ejs");
 
 /** 4-ROUTERS **/
-app.use('/admin', routerAdmin); // SSR: EJS
-app.use('/', router); //SPA: (REACT)
+app.use("/admin", routerAdmin); // SSR: EJS
+app.use("/", router); //SPA: (REACT)
 
-export default app;
+const server = http.createServer(app);
+const io = new ServerIOServer(server, {
+  cors: {
+    origin: true,
+    credentials: true,
+  },
+});
+
+let summaryClient = 0;
+io.on("connection", socket => {
+  summaryClient++;
+  console.log(`Connection & total [${summaryClient}]`);
+
+  socket.on("disconnect", () => {
+    summaryClient--;
+    console.log(`Disconnection & total [${summaryClient}]`);
+  });
+});
+
+export default server;
--- src/server.ts
+++ src/server.ts
@@ -2,19 +2,18 @@ import dotenv from "dotenv";
 dotenv.config();
 
 import mongoose from "mongoose";
-import app from "./app";
+import server from "./app";
 
-
-mongoose.connect(process.env.MONGO_URL as string, {})
-    .then((data) => {
-        
-        console.log("MongoDB connection succeed");
-        const PORT = process.env.PORT ?? 3003;
-        app.listen(PORT, () => {
-            console.info(`The server is running successfuly on port: ${PORT}, http://localhost:${PORT}`);
-            console.info(`Admin project on http://localhost:${PORT}/admin \n`);
-        })
-    })
-    .catch((err) => console.log("ERROR on connection MongoDB", err));
-
-
+mongoose
+  .connect(process.env.MONGO_URL as string, {})
+  .then(data => {
+    console.log("MongoDB connection succeed");
+    const PORT = process.env.PORT ?? 3003;
+    server.listen(PORT, () => {
+      console.info(
+        `The server is running successfuly on port: ${PORT}, http://localhost:${PORT}`
+      );
+      console.info(`Admin project on http://localhost:${PORT}/admin \n`);
+    });
+  })
+  .catch(err => console.log("ERROR on connection MongoDB", err));
